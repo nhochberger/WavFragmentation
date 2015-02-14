10ac541681c98a562dfa23b648e79ea12dd683cf
nhochberger <nico@irrwisch.net>
2015-02-14 12:05:51 +0100

    basic infrastructure finished

--- src/controller/WavFragmentationApplication.java
+++ src/controller/WavFragmentationApplication.java
@@ -6,7 +6,9 @@ import hochberger.utilities.application.session.BasicSession;
 import hochberger.utilities.eventbus.EventBus;
 import hochberger.utilities.eventbus.SimpleEventBus;
 import model.WavFragmenter;
+import model.WavPlayer;
 import model.events.BeginFragmentationEvent;
+import model.events.PlayWavEvent;
 import view.WavFragmentationGui;
 
 public class WavFragmentationApplication extends BasicLoggedApplication {
@@ -15,6 +17,7 @@ public class WavFragmentationApplication extends BasicLoggedApplication {
 	private final EventBus eventBus;
 	private final BasicSession session;
 	private final WavFragmenter wavFragmenter;
+	private final WavPlayer wavPlayer;
 
 	public static void main(final String[] args) {
 		setUpLoggingServices(WavFragmentationApplication.class);
@@ -34,7 +37,10 @@ public class WavFragmentationApplication extends BasicLoggedApplication {
 		this.session = new BasicSession(properties, this.eventBus, logger());
 		this.gui = new WavFragmentationGui(this.session);
 		this.wavFragmenter = new WavFragmenter(this.session);
+		this.wavPlayer = new WavPlayer(this.session);
+
 		this.eventBus.register(this.wavFragmenter, BeginFragmentationEvent.class);
+		this.eventBus.register(this.wavPlayer, PlayWavEvent.class);
 
 		Runtime.getRuntime().addShutdownHook(new Thread("Shutdown Hook") {
 			@Override
--- src/model/WavPlayer.java
+++ src/model/WavPlayer.java
@@ -1,14 +1,30 @@
 package model;
 
+import hochberger.utilities.application.session.BasicSession;
+import hochberger.utilities.application.session.SessionBasedObject;
 import hochberger.utilities.eventbus.EventReceiver;
+import model.events.MessageDisplayEvent;
 import model.events.PlayWavEvent;
 
-public class WavPlayer implements EventReceiver<PlayWavEvent> {
+public class WavPlayer extends SessionBasedObject implements EventReceiver<PlayWavEvent> {
 
-	@Override
-	public void receive(PlayWavEvent event) {
-		// TODO Auto-generated method stub
-
+	public WavPlayer(final BasicSession session) {
+		super(session);
 	}
 
+	@Override
+	public void receive(final PlayWavEvent event) {
+		playFromBuffer();
+	}
+
+	private void playFromBuffer() {
+		session().getEventBus().publish(new MessageDisplayEvent("Replaying wav from buffer"));
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		session().getEventBus().publish(new MessageDisplayEvent("Replay finished."));
+	}
 }
